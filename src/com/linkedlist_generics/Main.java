922a6098ff8443f19304e478657a5fa5145f3c43
Amit <amitlpalandurkar1998@gmail.com>
2023-07-07 00:00:55 +0530

    createing a simple Linked List of 56, 30 and 70.

--- src/com/linkedlist_generics/Main.java
+++ src/com/linkedlist_generics/Main.java
@@ -3,5 +3,11 @@ package com.linkedlist_generics;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Wellcome Data Structure Problems using Java Generics Project.");
+
+        LinkedListGenerics lgList = new LinkedListGenerics();
+        lgList.pushValue(70);
+        lgList.pushValue(30);
+        lgList.pushValue(56);
+
     }
 }
---
+++ src/com/linkedlist_generics/Node.java
@@ -0,0 +1,10 @@
+package com.linkedlist_generics;
+
+public class Node {
+    int data;
+    Node next;
+    Node(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
